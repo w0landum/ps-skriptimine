e4006a2511b5b1d761be18923a8002ec69fbce0b
Roland Sinirahu <Roland.sinirahu@voco.ee>
2024-11-11 04:28:59 -0800

    Practice 1 - using += operator

---
+++ pr1.ps1
@@ -0,0 +1,14 @@
+﻿# define data arrays
+$array1 = @(1, 2, 3)
+$array2 = @(4, 5, 6)
+
+# define empty result array
+$array3 = @()
+
+# add new values to empty array
+$array3 += $array1[0] + $array2[0]
+$array3 += $array1[1] + $array2[1]
+$array3 += $array1[2] + $array2[2]
+
+# output result array
+$array3
