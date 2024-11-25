99130a756d0177446009bb79fe641afe7de50d81
Roland Sinirahu <Roland.sinirahu@voco.ee>
2024-11-25 03:43:59 -0800

    Tsükli rakendamine

---
+++ pr7.ps1
@@ -0,0 +1,9 @@
+﻿$groups = @('Red', 'Green', 'Yellow', 'Blue')
+$studentGroups = @()
+
+for ($i = 1; $i -le 20; $i++){
+    $group = $groups | Get-Random
+    $studentGroups += @{'RollNumber' =$i;  'Group'=$group}
+}
+
+$studentGroups
