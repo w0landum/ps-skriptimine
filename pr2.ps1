9e038e37804868fb60c6d7bf6de4880553782845
Roland Sinirahu <Roland.sinirahu@voco.ee>
2024-11-30 09:30:10 -0800

    Praktika 2 HashTable'i tundmaõppimine

---
+++ pr2.ps1
@@ -0,0 +1,10 @@
+﻿$ht1 = @{"Name" = "John", "Joe", "Mary"; "DaysWorked" = 12, 20, 18}
+$ht2 = @{"Name" = "John", "Joe", "Mary"; "SalaryPerDay" = 100, 120, 150}
+
+$JohnSalary = $ht1.DaysWorked[0] * $ht2.SalaryPerDay[0]
+$JoeSalary = $ht1.DaysWorked[1] * $ht2.SalaryPerDay[1]
+$MarySalary = $ht1.DaysWorked[2] * $ht2.SalaryPerDay[2]
+
+$ht3 = @{Name=$ht1.Name[0], $ht1.Name[1], $ht3.Name[2]; Salary=$JohnSalary, $JoeSalary, $MarySalary}
+
+$ht3
