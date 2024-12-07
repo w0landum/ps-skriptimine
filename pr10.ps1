39fe4e00acc3adbd488a50c72e25ee8817b02fb8
Roland Sinirahu <Roland.sinirahu@voco.ee>
2024-12-07 10:41:13 -0800

    Until-tsükkel.

---
+++ pr10.ps1
@@ -0,0 +1,15 @@
+﻿$ktl = 0
+do
+{
+$k = $n
+$k = Get-Process | ?{$_.name -like "note*"}
+if($k -ne $null)
+{$kt2 = 2
+Write-Host "Notepad töötab."
+Start-Sleep -Seconds 1
+$kt1++
+}
+else
+{$kt2 = 1}
+}until($kt2 -eq 1)
+$kt1
