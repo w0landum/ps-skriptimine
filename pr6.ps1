e927f6b7e5357474c742f4652603012036a2afdd
Roland Sinirahu <Roland.sinirahu@voco.ee>
2024-12-07 10:06:23 -0800

    CSV-failist lugemine ja foreach tsükkel.

---
+++ pr6.ps1
@@ -0,0 +1,23 @@
+﻿$csv = Import-Csv C:\temp\opilased.csv
+
+$tulemus = @()
+
+ foreach($r in $csv)
+    {
+      if([int]$r.Vanus -ge 4 -and [int]$r.Vanus -le 10)
+       {
+         $kool = "Noorem"
+       }
+      elseif([int]$r.Vanus -ge 11 -and [int]$r.Vanus -le 17)
+       {
+         $kool = "Vanem"
+       }
+      $vorm = [PSCustomObject]@{
+          Nimi = $r.Nimi
+          Kool = $kool
+           }
+      $tulemus += $vorm
+    }
+
+$tulemus
+
