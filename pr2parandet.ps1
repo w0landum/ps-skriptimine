a1105f635016a3db79f3bdf7bb00403e4b52bc57
Roland Sinirahu <Roland.sinirahu@voco.ee>
2025-01-11 04:01:42 -0800

    Parandatud ülesanne 2.

---
+++ pr2parandet.ps1
@@ -0,0 +1,12 @@
+﻿#Write-Host "I want to learn Powershell!" -ForegroundColor Yellow
+
+$hash1=@{Name="John","Joe","Mary";DaysWorked=12,20,18}
+$hash2=@{Name="John","Joe","Mary";SalaryPerDay=100,120,150}
+
+$JohnSalary=$hash1.DaysWorked[0]*$hash2.SalaryPerDay[0]
+$JoeSalary=$hash1.DaysWorked[1]*$hash2.SalaryPerDay[1]
+$MarySalary=$hash1.DaysWorked[2]*$hash2.SalaryPerDay[2]
+
+$hash3=
+@{Name=$hash1.Name[0],$hash1.Name[1],$hash1.Name[2];Salary=$JohnSalary,$JoeSalary,$MarySalary}
+$hash3
