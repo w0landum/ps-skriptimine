d1b66d23c7cabf7026604be7cae69f70a94076f5
Roland Sinirahu <Roland.sinirahu@voco.ee>
2025-01-30 05:25:17 -0800

    Lokaalse kasutaja loomine tema ees- ja perekonnanime järgi.

---
+++ lokaalse_kasutaja_loomine.ps1
@@ -0,0 +1,18 @@
+﻿$eesnimi = Read-Host 'Palun sisesta oma eesnimi '
+$perekonnanimi = Read-Host 'Palun sisesta oma perekonnanimi '
+
+$kasutajanimi = $eesnimi.ToLower() + "." + $perekonnanimi.ToLower()
+$parool = 'Parool1!'
+$taisnimi = $eesnimi, $perekonnanimi
+$kirje = "Lokaalne kasutaja"
+
+Write-Host 'Loodav kasutajanimi on' $kasutajanimi
+
+$kasutajaeksisteerib = Get-LocalUser $kasutajanimi -ErrorAction SilentlyContinue
+
+if(($kasutajanimi | Select Name).Name.Count -ge 1){
+    Write-Host 'Ei ole võimalik kasutajat luua - sama nimega konto juba eksisteerib.'
+    Exit
+}else {
+   echo "Teie kasutajanimi: " $kasutajanimi "Teie parool: " $parool "Teie täisnimi: " $taisnimi "Teine kasutajatüüp: " $kirje
+}
